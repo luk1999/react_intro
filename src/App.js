cb167f5b2db1431657cf7fec92d515b4d801c145
Łukasz Stuszek <Lukasz.Stuszek@commerzbank.com>
2025-08-20 07:31:17 +0000

    Chapter 1: Introduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+const ToggleText = ({ buttonLabel, text }) => {
+    const [showText, setShowText] = useState(false);
+
+    const toggleText = () => setShowText(!showText);
+
+    return (
+        <div>
+            <button type="button" onClick={toggleText}>
+                {buttonLabel}
+            </button>
+            {showText && <p>{text}</p>}
+        </div>
+    );
+};
+
+export default function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <header className="App-header">React Tutorial App</header>
+            <h1>Hello world!</h1>
+            <ToggleText
+                buttonLabel="Toggle text 1"
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            ></ToggleText>
+            <ToggleText
+                buttonLabel="Toggle text 2"
+                text="In id scelerisque nisl. Curabitur eu nisl eget orci dapibus maximus. "
+            ></ToggleText>
+            <ToggleText
+                buttonLabel="Toggle text 3"
+                text="Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas."
+            ></ToggleText>
         </div>
     );
 }
-
-export default App;
